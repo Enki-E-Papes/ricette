2da7f05678660314f3b25d46452976ce2f3d88fd
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2023-03-15 13:26:43 +0000

    commit2

--- uno.py
+++ uno.py
@@ -5,6 +5,22 @@ app = Flask(__name__)   #variabile che identifica il sito web
 def ricetteHomeuno():
     return render_template('./ricette.html')
 
+@app.route('/CosceDiPolloGratinate', methods=['GET'])
+def CosceDiPolloGratinate():
+    return render_template('./CosceDiPolloGratinate.html')
+
+@app.route('/MedaglioniDiFormaggio', methods=['GET'])
+def MedaglioniDiFormaggio():
+    return render_template('./MedaglioniDiFormaggio.html')
+
+@app.route('/platessaPanata', methods=['GET'])
+def platessaPanata():
+    return render_template('./platessaPanata.html')
+
+@app.route('/PolpetteDiCarne', methods=['GET'])
+def PolpetteDiCarne():
+    return render_template('./PolpetteDiCarne.html')
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)    #fà partire il programma
+  app.run(host='0.0.0.0', port=8080, debug=True)    #fà partire il programma
